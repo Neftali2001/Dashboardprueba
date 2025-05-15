75736bccee6439c7471d5363aff1eca320b28e9e
Neftali <meftalio@gmail.com>
2025-05-15 17:31:42 -0600

    cambios de login

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-
 // middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
@@ -9,17 +8,17 @@ export function middleware(request: NextRequest) {
 
   if (!token && !isAuthPage) {
     // Redirigir a la página de inicio de sesión si no está autenticado
-    return NextResponse.redirect(new URL('/auth/signin', request.url));
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   if (token && isAuthPage) {
     // Redirigir al dashboard si ya está autenticado y accede a la página de autenticación
-    return NextResponse.redirect(new URL('/dashboard/(overview)', request.url));
+     return NextResponse.redirect(new URL('/dashboard/(overview)', request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/profile/:path*', '/auth/:path*'],
+  matcher: ['/dashboard/(overview)', '/profile/:path*', '/auth/:path*, /login'],
 };
